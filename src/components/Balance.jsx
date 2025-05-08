9a285a662b5d4f89d458a5ae3ff7167ff58936c0
Mohamed Medhat <mohamedsheetat@gmail.com>
2025-05-08 21:44:37 +0300

    Balance component

---
+++ src/components/Balance.jsx
@@ -0,0 +1,40 @@
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { FaWallet } from "react-icons/fa6";
+import { FaCircleArrowUp } from "react-icons/fa6";
+
+
+export default function Balance() {
+    return (
+        <Flex w={'full'} direction={'column'} boxShadow={'md'}  >
+                <Box w={'full'} py={3} px={8} bg={'#4299e1'}  >
+                    <Flex  w={'fit'} align={'center'}>
+                    <FaWallet color="white" />
+                    <Text  fontWeight={'bold'} fontSize={'2xl'} color={'white'} ml={3} as={'span'} > Total Balance </Text>
+                    </Flex>
+                </Box>
+                <Box py={3} px={8} >
+                    <Heading size={'3xl'} >$0.00</Heading>
+                </Box>
+
+                <Flex py={3} px={8} gap={10} justify={'flex-start'} >
+
+                    <Flex  gap={2} alignItems={'center'} >
+                        <FaCircleArrowUp   color="green" />
+
+                        <Box>
+                        <Text>Monthly Income</Text>
+                        <Text>$0.00</Text>
+                        </Box>
+                    </Flex>
+
+                    <Flex gap={2} alignItems={'center'} >  
+                        <FaCircleArrowUp color="red" />
+                        <Box>
+                        <Text>Monthly Expenses</Text>
+                        <Text>$0.00</Text>
+                        </Box>
+                    </Flex>
+                </Flex>
+        </Flex>
+    )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
+import Balance from "../../components/Balance";
 import Header from "../../components/Header";
 
 export default function Home() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Balance/>
+    </>
+    
   )
 }
